dddc6fa3a37e3582a13fa7f98a9a100208bfd1ad
Christos Paschalidis <paschalidi@outlook.com>
2020-02-02 23:52:40 +0100

    updates packages

--- exercises/beginner/render/05/src/index.js
+++ exercises/beginner/render/05/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import ExerciseDescription from './ExerciseDescription';
+import {ExerciseDescription, SolutionArea, render, GlobalStyles} from 'react-exercises-ui'
 import PremierLeagueFixtures from './PremierLeagueFixtures';
-
-import './styles.css';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <ExerciseDescription
-          level="Beginner"
-          topic="Render"
-          exerciseNumber="5"
-          title="Dealing with nested selectors"
-          exerciseComponentName="PremierLeagueFixtures"
-        />
-      </div>
-      <h3>
-        The React component in PremierLeagueFixtures.js is rendered below:
-      </h3>
-      <hr />
-      <PremierLeagueFixtures />
+      <GlobalStyles/>
+      <ExerciseDescription {...render.ex05}/>
+      <SolutionArea component={<PremierLeagueFixtures />} />
+
     </>
   );
 }
