961a37ead3a79b670d00097f35896087ce00f9ca
Christos Paschalidis <paschalidi@outlook.com>
2020-03-04 13:45:31 +0100

    Update BasketballStar.spec.js

--- exercises/beginner/render/03/src/BasketballStar.spec.js
+++ exercises/beginner/render/03/src/BasketballStar.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import BasketballStar from './BasketballStar';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
