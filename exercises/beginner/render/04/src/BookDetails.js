dff4b02676d2512ce08b9e8c226533facdc70a9c
Christos Paschalidis <paschalidi@outlook.com>
2020-03-04 13:48:26 +0100

    Update BookDetails.js

--- exercises/beginner/render/04/src/BookDetails.js
+++ exercises/beginner/render/04/src/BookDetails.js
@@ -3,9 +3,6 @@ import React from 'react';
 function BookDetails() {
   return (
     <div>
-      <div>32156</div>
-      <div>JavaScript: The Good Parts</div>
-      <author />
     </div>
   );
 }
